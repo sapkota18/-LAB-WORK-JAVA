69a672eef602597cd6c193e861aedbcdc18538ed
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-03-07 20:13:34 +0545

    Small change

--- Rectangle.java
+++ Rectangle.java
@@ -6,15 +6,15 @@ public class Rectangle {
         breadth = b;
     }
 
-    public int returnarea() {
+    public int Area() {
         return length * breadth;
     }
 
     public static void main(String[] args) {
         Rectangle R1 = new Rectangle(4, 5);
         Rectangle R2 = new Rectangle(5, 8);
-        int area1 = R1.returnarea();
-        int area2 = R2.returnarea();
+        int area1 = R1.Area();
+        int area2 = R2.Area();
         System.out.println("Area of of Rectangle1: " + area1);
         System.out.println("Area of of Rectangle2: " + area2);
     }

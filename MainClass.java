890b1b9b0d1e71b116d02fad049db4ae09b34805
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-03-18 18:08:37 +0545

    LAB N0.20

---
+++ MainClass.java
@@ -0,0 +1,21 @@
+class ParentClass {
+    public void parentMethod() {
+        System.out.println("This is parent class");
+    }
+}
+
+class ChildClass extends ParentClass {
+    public void childMethod() {
+        System.out.println("This is child class");
+    }
+}
+
+public class MainClass {
+    public static void main (String[] args) {
+        ParentClass parentObj = new ParentClass();
+        ChildClass childObj = new ChildClass();
+        parentObj.parentMethod();
+        childObj.childMethod();
+        childObj.parentMethod();
+    }
+}

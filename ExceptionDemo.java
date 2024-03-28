4b09392ce8200a5282327ad06f09429979a11aca
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-03-28 20:17:05 +0545

    lab work no.26

---
+++ ExceptionDemo.java
@@ -0,0 +1,19 @@
+public class ExceptionDemo {
+
+    public static void main(String[] args) {
+        String str = null;
+        try {
+            int length = str.length(); 
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException: " + e);
+        }
+
+        // Demonstrating NumberFormatException
+        String numStr = "abc";
+        try {
+            int num = Integer.parseInt(numStr);
+        } catch (NumberFormatException e) {
+            System.out.println("Caught NumberFormatException: " + e);
+        }
+    }
+}

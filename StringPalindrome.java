df69ff263c8b3e13c7e8134a3f738a7a3bf1f960
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-04-05 23:04:03 +0545

    LAB WORk 29

---
+++ StringPalindrome.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StringPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        StringBuffer sb = new StringBuffer(str);
+        sb.reverse();
+        String reversedStr = sb.toString();
+        if (str.equals(reversedStr)) {
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
+    }
+}

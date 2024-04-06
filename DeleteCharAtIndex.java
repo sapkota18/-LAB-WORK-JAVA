75eec5a8fe5359683afad7dca7e653a7d2a07830
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-04-06 19:21:32 +0545

    modified lab no.31

--- DeleteCharAtIndex.java
+++ DeleteCharAtIndex.java
@@ -8,7 +8,7 @@ public class DeleteCharAtIndex {
             StringBuffer s = new StringBuffer(str);
             System.out.println("Character at index 3: " + str.charAt(3));
             s.deleteCharAt(3);
-            System.out.println("String after deleting character at inde 3: " + s.toString());
+            System.out.println("String after deleting character at index 3: " + s.toString());
         } else {
             System.out.println("Input string should have at least 4 characters.");
         }

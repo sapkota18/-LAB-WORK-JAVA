beb6afe3575cebb8d2eca1849ce5e14b5ca74ea5
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-04-08 18:44:44 +0545

    Changes made in lab no.33

--- SynchronizationExample.java
+++ SynchronizationExample.java
@@ -1,7 +1,7 @@
 class MultiplicationTable{
     public synchronized void printTable(int n){
         for(int i = 1; i <= 10; i++) {
-            System.out.println(n + "*" + i + "=" + (n * i));
+            System.out.printS(n + "*" + i + "=" + (n * i));
             try {
                 Thread.sleep(400);
             } catch(Exception e) {

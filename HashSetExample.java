efcd7a797ced892772d88de25d09c1552e7e41ff
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-05-06 09:27:40 +0545

    LAB N0.44

---
+++ HashSetExample.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+public class HashSetExample {
+    public static void main(String[] args) {
+        HashSet<String> cources = new HashSet<String>();
+        cources.add("BCA");
+        cources.add("BBA");
+        cources.add("BSCCSIT");
+        cources.add("BE");
+        System.out.println("Cources in the HashSet: "+cources);
+
+    }
+}

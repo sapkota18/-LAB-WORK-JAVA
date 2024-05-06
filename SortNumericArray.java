1e985670f09c6c3f1e5401d4161235dd32e4f26e
sapkota18 <159790081+sapkota18@users.noreply.github.com>
2024-05-06 08:51:51 +0545

    LAB NO.41

---
+++ SortNumericArray.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class SortNumericArray {
+    public static void main(String[] args) {
+        int arr[]={0,4,8,45,6,23,21};
+        System.out.println("Original Array: "+Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println("Sorted Array: "+Arrays.toString(arr));
+    }
+}
